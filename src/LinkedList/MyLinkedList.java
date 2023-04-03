d42979417ad6b322e72f69a1fa12927a663ddcd0
tuuyen09 <tu190901@gmail.com>
2023-04-03 13:58:08 +0700

    LinkedList

--- src/LinkedList/MyLinkedList.java
+++ src/LinkedList/MyLinkedList.java
@@ -1,2 +1,53 @@
-package LinkedList;public class MyLinkedList {
+package LinkedList;
+
+import org.w3c.dom.Node;
+
+public class MyLinkedList {
+    private Node head;
+    private int numNode;
+    public MyLinkedList(Object data){
+        head = new Node (data);
+        numNode ++;
+    }
+    private class Node {
+        private Node next;
+        private Object data;
+        public Node (Object data){
+            this.data = data;
+        }
+        public Object getData(){
+            return this.data;
+        }
+    }
+    public void add (int index, Object data){
+        Node temp = head;
+        Node holder;
+        for (int i = 0; i < index -1 && temp.next != null; i ++){
+            temp = temp.next;
+        }
+        holder = temp.next;
+        temp.next = new Node(data);
+        temp.next.next = holder;
+        numNode++;
+    }
+    public void addFirst (Object data){
+        Node temp = head;
+        head = new Node(data);
+        head.next = temp;
+        numNode++;
+    }
+    public Node get(int index){
+        Node temp = head;
+        for (int i = 0; i<index; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+    public void printList(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
 }
